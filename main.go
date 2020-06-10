9b0fbfe80f4d2c64ca185f63859b0f0b696ae63f
machariamarigi <machariamarigi@gmail.com>
2020-06-10 16:27:48 +0300

    simple server setup

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+	"log"
+	"io/ioutil"
+)
+
+func main()  {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Hello World")
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, "Oops", http.StatusBadRequest)
+			return
+		}
+
+		log.Printf("Data %s\n", d)
+	})
+
+	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
+		log.Println("Goodbye World")
+	})
+
+	http.ListenAndServe(":9090", nil);
+}
