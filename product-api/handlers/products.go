def5e916e8729156d6683bc668d3da1932298d98
machariamarigi <machariamarigi@gmail.com>
2020-06-18 23:41:55 +0300

    product handler can update products

--- product-api/handlers/products.go
+++ product-api/handlers/products.go
@@ -29,7 +29,37 @@ func (p*Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// handle PUT
+	if r.Method == http.MethodPut {
+		p.l.Println("PUT", r.URL.Path)
+	
+		reg := regexp.MustCompile(`/([0-9]+)`)
+		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
 
+		if len(g) != 1 {
+			p.l.Println("Invalid URI more than one id")
+			http.Error(rw, "Invalid URL", http.StatusBadRequest)
+			return
+		}
+
+		if len(g[0]) != 2 {
+			p.l.Println("Invalid URI more than one capture group")
+			http.Error(rw, "Invalid URL", http.StatusBadRequest)
+			return
+		}
+
+		idString := g[0][1]
+		id, err := strconv.Atoi(idString)
+
+		if err != nil {
+			p.l.Println("Invalid URI unable to convert to numer", idString)
+			http.Error(rw, "Invalid URL", http.StatusBadRequest)
+			return
+		}
+
+		p.updateProduct(id, rw, r)
+		return
+	}
 
 	// catch all
 	rw.WriteHeader(http.StatusMethodNotAllowed)
@@ -52,11 +82,34 @@ func (p*Products) addProduct(rw http.ResponseWriter, r *http.Request) {
 	p.l.Println("Handle POST Products")
 
 	prod := &data.Product{}
-	err := prod.FromJSON(r.Body)
 
+	err := prod.FromJSON(r.Body)
 	if err != nil {
 		http.Error(rw, "Unable to decode JSON", http.StatusBadRequest)
 	}
 
 	data.AddProducts(prod)
+}
+
+func (p Products) updateProduct(id int, rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle PUT Product")
+
+	prod := &data.Product{}
+
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to decode JSON", http.StatusBadRequest)
+	}
+
+	err = data.UpdateProduct(id, prod)
+
+	if err == data.ErrorProductNotFound {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(rw, "Something went wrong", http.StatusInternalServerError)
+		return
+	}
 }
