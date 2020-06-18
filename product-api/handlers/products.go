75c2d90b34994b94d8970c984a6d975949e713fc
machariamarigi <machariamarigi@gmail.com>
2020-06-18 16:38:45 +0300

    handle POST product

--- product-api/handlers/products.go
+++ product-api/handlers/products.go
@@ -4,6 +4,8 @@ import (
 	"github.com/machariamarigi/ju0920/product-api/data"
 	"net/http"
 	"log"
+	"regexp"
+	"strconv"
 )
 
 type Products struct {
@@ -15,22 +17,46 @@ func NewProducts(l *log.Logger) *Products {
 }
 
 func (p*Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	// handle get
+	// handle GET
 	if r.Method == http.MethodGet {
 		p.getProducts(rw, r)
 		return
 	}
 
-	// handlePut
+	// handle POST
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
+
+
 
 	// catch all
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
 func (p*Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle GET Products")
+
+	// fetch the products from the datastore
 	pl := data.GetProducts()
+
+	// serailize the list to JSON
 	err := pl.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "unable to encode json", http.StatusInternalServerError)
 	}
+}
+
+func (p*Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST Products")
+
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+
+	if err != nil {
+		http.Error(rw, "Unable to decode JSON", http.StatusBadRequest)
+	}
+
+	data.AddProducts(prod)
 }
