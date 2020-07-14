aca306500e2e87b7936f0e79bb321587ecb340fb
machariamarigi <machariamarigi@gmail.com>
2020-07-14 15:37:27 +0300

    generate swagger docs for POST /products/{id}

--- product-api/handlers/add.go
+++ product-api/handlers/add.go
@@ -6,6 +6,14 @@ import (
 	"github.com/machariamarigi/ju0920/product-api/data"
 )
 
+// swagger:route POST /products products createProduct
+// Create a new product
+//
+// responses:
+//	200: productResponse
+// 	422: errorValidation
+// 	501: errorResponse
+
 // Create handles POST requests for adding a new product
 func (products*Products) Create(responseWriter http.ResponseWriter, request *http.Request) {
 	products.logger.Println("Handle POST Products")
