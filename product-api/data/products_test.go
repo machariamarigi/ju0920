82f1aeaed471693f34c77b744777d26deea3fa3f
machariamarigi <machariamarigi@gmail.com>
2020-07-20 16:23:07 +0300

    add tests

--- product-api/data/products_test.go
+++ product-api/data/products_test.go
@@ -2,18 +2,65 @@ package data
 
 import (
 	"testing"
+	"bytes"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestChecksValidation(t* testing.T) {
-	product := &Product{
-		Name: "Mash",
-		Price: 100,
-		SKU: "ods-ends-inbtns",
+func TestProductMissingNameReturnsError(testing *testing.T) {
+	product := Product{
+		Price: 1.22,
+		SKU:   "dffg-fgfg-dfdf",
 	}
 
-	err := product.Validate()
+	validation := NewValidation()
+	err := validation.Validate(product)
+	assert.Len(testing, err, 1)
+}
 
-	if err != nil {
-		t.Fatal(err)
+func TestProductMissingPriceReturnsError(testing *testing.T) {
+	product := Product{
+		Name:  "test product",
+		Price: -1,
+		SKU:   "dffg-fgfg-dfdf",
 	}
+
+	validation := NewValidation()
+	err := validation.Validate(product)
+	assert.Len(testing, err, 1)
+}
+
+func TestInvalidSKUReturnsError(testing *testing.T) {
+	product := Product{
+		Name:  "test product",
+		Price: 1.22,
+		SKU:   "dffg",
+	}
+	validation := NewValidation()
+	err := validation.Validate(product)
+	assert.Len(testing, err, 1)
+}
+
+func TestValidProductDoesNotReturnError(testing *testing.T) {
+	product := Product{
+		Name:  "test product",
+		Price: 1.23,
+		SKU:   "dffg-fgfg-dfdf",
+	}
+
+	validation := NewValidation()
+	err := validation.Validate(product)
+	assert.Len(testing, err, 0)
+}
+
+func TestProductToJSON(testing *testing.T) {
+	productSample := []*Product{
+		&Product{
+			Name: "test",
+		},
+	}
+
+	buffer := bytes.NewBufferString("")
+	err := ToJSON(productSample, buffer)
+	assert.NoError(testing, err)
 }
